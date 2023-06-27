e37105e273238ca8070866ef9eb8d42489019f03
joseamila <josea.miladelaroca@gmail.com>
2023-06-26 23:40:39 -0300

    first commit

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github} target="_blank">
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.linkedin && (
+          <li>
+            <a href={socialprofils.linkedin} target="_blank">
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+        {socialprofils.youtube && (
+          <li>
+            <a href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Follow Me</p>
+    </div>
+  );
+};
---
+++ src/content_option.js
@@ -0,0 +1,84 @@
+const logotext = "JM";
+const meta = {
+    title: "Jose Milà de la Roca",
+    description: "",
+};
+
+const introdata = {
+    title: "I’m Jose Milà de la Roca",
+    animated: {
+        first: "I love coding",
+        second: "I code websites",
+        third: "I develop mobile apps",
+    },
+    description: "(This site still in development)",
+    your_img_url: "/images/me.png",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I am an eternal student of software development. Always learning new ways to  write and integrate efficient and sustainable code. Passionate about challenges and always ready & eager for them.",
+};
+const worktimeline = [{
+        jobtitle: "Technical Support Analyst",
+        where: "Oracle",
+        date: "2022 - Currently",
+    },
+    {
+        jobtitle: "Frontend Developer",
+        where: "Metraigo",
+        date: "2023 - Currently",
+    },
+];
+
+const skills = [{
+        name: "React.js",
+        value: 90,
+    },
+    {
+        name: "Next.js",
+        value: 85,
+    },
+    {
+        name: "Vue.js",
+        value: 80,
+    },
+    {
+        name: "Node.js",
+        value: 60,
+    },
+];
+
+const dataportfolio = [{
+        img: "/images/metraigoLogo.png",
+        description: "Ecommerce focused on the provision of items purchased through Amazon to Venezuela.",
+        link: "https://www.metraigo.com",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "josea.miladelaroca@gmail.com",
+    YOUR_FONE: "(598) 126-421",
+    description: "Let's build cool stuff together.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Joseamila",
+    linkedin: "https://www.linkedin.com/in/jos%C3%A9-mil%C3%A1-de-la-roca-mayz-6694a0189/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
